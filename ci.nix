7e8fa40b588569d1dfb111f4b4ee46bafcf43865
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-11-04 10:01:57 +0100

    split up the nix build

--- ci.nix
+++ ci.nix
@@ -1,22 +1,9 @@
 let
   pkgs = import ./nix/pkgs.nix;
-  nix-pre-commit-hooks =
-    import (
-      builtins.fetchTarball {
-        url = "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/f709c4652d4696dbe7c6a8354ebd5938f2bf807b.tar.gz";
-        sha256 = "sha256:0700c5awc2gjzgikhx69vjbpyshx6b5xljmpxrdzpgqyg3blxbkl";
-      }
-    );
+  pre-commit-hooks = import ./nix/pre-commit.nix;
 
 in
 pkgs.intrayPackages // {
   inherit (pkgs) intrayNotification;
-  pre-commit-check = nix-pre-commit-hooks.run {
-    src = ./.;
-    hooks = {
-      nixpkgs-fmt.enable = true;
-      ormolu.enable = true;
-      hlint.enable = true;
-    };
-  };
+  pre-commit-check = pre-commit-hooks.run;
 }
--- shell.nix
+++ shell.nix
@@ -1,3 +1,24 @@
-(import ./nix/pkgs.nix).mkShell {
-  inherit ((import ./ci.nix).pre-commit-check) shellHook;
+let
+  pkgs = import ./nix/pkgs.nix;
+  pre-commit-hooks = import ./nix/pre-commit.nix;
+in
+pkgs.mkShell {
+  name = "intray-nix-shell";
+  buildInputs = pre-commit-hooks.tools;
+  shellHook = ''
+    ${pre-commit-hooks.run.shellHook}
+
+
+    function nix-build_ {
+      nix-build \
+        --option extra-substituters https://validity.cachix.org \
+        --option trusted-public-keys validity.cachix.org-1:CqZp6vt9ir3yB5f8GAtfkJxPZG8hKC5fhIdaQsf7eZE= \
+        --option extra-substituters https://mergeless.cachix.org \
+        --option trusted-public-keys mergeless.cachix.org-1:AFASeiTKF1aaIq8ZjqMF7xwPNKtKyEsUoU+8TgAwLsg= \
+        --option extra-substituters https://intray.cachix.org \
+        --option trusted-public-keys intray.cachix.org-1:qD7I/NQLia2iy6cbzZvFuvn09iuL4AkTmHvjxrQlccQ= \
+        $*
+    }
+    alias nix-build=nix-build_
+  '';
 }
