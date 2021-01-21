e0ab102d2646eeec5c9fe2d1b9235873213ebf6c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-01-21 17:32:02 +0100

    fixed newlines not being preserved in intray items

--- intray-web-server/test/Intray/Web/Server/Handler/AddRSpec.hs
+++ intray-web-server/test/Intray/Web/Server/Handler/AddRSpec.hs
@@ -2,6 +2,9 @@
 
 module Intray.Web.Server.Handler.AddRSpec where
 
+import Control.Monad.Reader
+import Intray.Client
+import Intray.Server.TestUtils as TestUtils
 import Intray.Web.Server.Foundation
 import Intray.Web.Server.TestUtils
 import Network.HTTP.Types
@@ -18,16 +21,37 @@ spec =
         $ do
           get AddR
           statusIs 200
-      yit "manages to add an item with a newline"
-        $ withExampleAccount_
-        $ do
-          get AddR
-          statusIs 200
-          request $ do
-            setMethod methodPost
-            setUrl AddR
-            addToken
-            addPostParam "contents" "hello\nworld"
-          statusIs 303
-          loc <- getLocation
-          liftIO $ loc `shouldBe` Right AddR
+      let roundtripTest contents =
+            yit ("roundtrips " <> show contents)
+              $ withExampleAccount
+              $ \un pw -> do
+                get AddR
+                statusIs 200
+                request $ do
+                  setMethod methodPost
+                  setUrl AddR
+                  addToken
+                  addPostParam "contents" contents
+                statusIs 303
+                loc <- getLocation
+                liftIO $ loc `shouldBe` Right AddR
+                man <- asks $ appHttpManager . yesodClientSite
+                burl <- asks $ appAPIBaseUrl . yesodClientSite
+                let cenv = ClientEnv man burl Nothing
+                liftIO $ do
+                  token <- TestUtils.login cenv un pw
+                  runClientOrError cenv $ do
+                    uuids <- clientGetItemUUIDs token
+                    case uuids of
+                      [uuid] -> do
+                        ii <- clientGetItem token uuid
+                        liftIO $ itemInfoContents ii `shouldBe` textTypedItem contents
+                      _ -> liftIO $ expectationFailure "expected exactly one item"
+      roundtripTest "hello"
+      roundtripTest "hello\n"
+      roundtripTest "hello\nworld"
+      roundtripTest "hello\nworld\n"
+      roundtripTest "hello\n\nworld"
+      roundtripTest "hello\n\nworld\n"
+      roundtripTest "hello\r\nworld"
+      roundtripTest "hello\r\nworld\n"
