12c52e51bfb60d8d88eeed7ece1a81031ed0742f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-01 21:22:48 +0200

    make then nixos module test more realistic

--- nix/nixos-module-test.nix
+++ nix/nixos-module-test.nix
@@ -4,13 +4,7 @@
 }:
 let
   intray-production = import (./nixos-module.nix) { inherit sources; inherit pkgs; envname = "production"; };
-  home-manager = import (
-    builtins.fetchTarball
-      {
-        url = "https://github.com/rycee/home-manager/archive/472ca211cac604efdf621337067a237be9df389e.tar.gz";
-        sha256 = "sha256:1gbfsnd7zsxwqryxd4r6jz9sgdz6ghlkapws1cdxshrbxlwhqad1";
-      } + "/nixos/default.nix"
-  );
+  home-manager = import (sources.home-manager + "/nixos/default.nix");
 
   api-port = 8001;
   web-port = 8002;
@@ -18,31 +12,37 @@ in
 pkgs.nixosTest (
   { lib, pkgs, ... }: {
     name = "intray-module-test";
-    machine = {
-      imports = [
-        intray-production
-        home-manager
-      ];
-      services.intray.production = {
-        enable = true;
-        inherit api-port;
-        inherit web-port;
-        log-level = "LevelDebug";
-      };
-      users.users.testuser.isNormalUser = true;
-      home-manager.users.testuser = { pkgs, ... }: {
+    nodes = {
+      server = {
         imports = [
-          ./home-manager-module.nix
+          intray-production
         ];
-        xdg.enable = true;
-        home.stateVersion = "20.09";
-        programs.intray = {
+        services.intray.production = {
           enable = true;
-          sync = {
+          inherit api-port;
+          inherit web-port;
+          log-level = "LevelDebug";
+        };
+      };
+      client = {
+        imports = [
+          home-manager
+        ];
+        users.users.testuser.isNormalUser = true;
+        home-manager.users.testuser = { pkgs, ... }: {
+          imports = [
+            ./home-manager-module.nix
+          ];
+          xdg.enable = true;
+          home.stateVersion = "20.09";
+          programs.intray = {
             enable = true;
-            url = "localhost:${builtins.toString api-port}";
-            username = "testuser";
-            password = "testpassword";
+            sync = {
+              enable = true;
+              url = "server:${builtins.toString api-port}";
+              username = "testuser";
+              password = "testpassword";
+            };
           };
         };
       };
@@ -50,22 +50,25 @@ pkgs.nixosTest (
     testScript = ''
       from shlex import quote
 
-      machine.wait_for_unit("multi-user.target")
+      server.start()
+      client.start()
+      server.wait_for_unit("multi-user.target")
+      client.wait_for_unit("multi-user.target")
 
-      machine.wait_for_unit("intray-production.service")
-      machine.wait_for_open_port(${builtins.toString api-port})
-      machine.succeed("curl localhost:${builtins.toString api-port}")
-      machine.wait_for_open_port(${builtins.toString web-port})
-      machine.succeed("curl localhost:${builtins.toString web-port}")
+      server.wait_for_unit("intray-production.service")
+      server.wait_for_open_port(${builtins.toString api-port})
+      client.succeed("curl server:${builtins.toString api-port}")
+      server.wait_for_open_port(${builtins.toString web-port})
+      client.succeed("curl server:${builtins.toString web-port}")
 
-      machine.wait_for_unit("home-manager-testuser.service")
+      client.wait_for_unit("home-manager-testuser.service")
 
 
       def su(user, cmd):
           return f"su - {user} -c {quote(cmd)}"
 
 
-      machine.succeed(su("testuser", "cat ~/.config/intray/config.yaml"))
+      client.succeed(su("testuser", "cat ~/.config/intray/config.yaml"))
 
       # Something is wrong such that the register command hangs, for unknown reasons
       # machine.succeed(su("testuser", "intray register"))
