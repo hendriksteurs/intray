3c29a0bbebecb3be96da7a07ece4ae6e94b761c2
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-07-08 16:37:45 +0200

    Fix the CTA, fixes #19

--- intray-web-server/test/Intray/Web/Server/Handler/HomeRSpec.hs
+++ intray-web-server/test/Intray/Web/Server/Handler/HomeRSpec.hs
@@ -8,13 +8,25 @@ import TestImport
 spec :: Spec
 spec =
   intrayWebServerSpec $
-    ydescribe "HomeR" $
-      do
-        yit "gets a 200 for non-logged-in user" $ do
+    ydescribe "HomeR" $ do
+      yit "gets a 200 for non-logged-in user" $ do
+        get HomeR
+        statusIs 200
+
+      yit "gets a 200 for an example user" $
+        withExampleAccount_ $ do
           get HomeR
           statusIs 200
-        yit "gets a 200 for an example user" $
-          withExampleAccount_ $
-            do
-              get HomeR
-              statusIs 200
+
+      yit "gets a login page when clicking on the CTA" $ do
+        get HomeR
+        statusIs 200
+        get AccountR
+        statusIs 303
+
+      yit "gets to the account page when clicking on the CTA when already logged in" $ do
+        withExampleAccount_ $ do
+          get HomeR
+          statusIs 200
+          get AccountR
+          statusIs 200
