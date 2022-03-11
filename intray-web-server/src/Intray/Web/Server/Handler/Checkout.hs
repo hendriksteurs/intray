d0217181e9e241da883177258561579f1ac2b75b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-03-11 23:34:04 +0100

    found the missing piece

--- intray-web-server/src/Intray/Web/Server/Handler/Checkout.hs
+++ intray-web-server/src/Intray/Web/Server/Handler/Checkout.hs
@@ -1,11 +1,33 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Intray.Web.Server.Handler.Checkout
-  ( getCheckoutSuccessR,
+  ( getCheckoutR,
+    getCheckoutSuccessR,
     getCheckoutCanceledR,
   )
 where
 
+import Intray.Client
 import Intray.Web.Server.Foundation
 import Yesod
+
+getCheckoutR :: Handler Html
+getCheckoutR = do
+  withLogin $ \t -> do
+    mMonetisation <- runClientOrErr clientGetMonetisation
+    status <- runClientOrErr $ clientGetUserSubscription t
+    renderUrl <- getUrlRender
+    InitiatedCheckoutSession {..} <-
+      runClientOrErr $
+        clientPostInitiateStripeCheckoutSession
+          t
+          InitiateStripeCheckoutSession
+            { initiateStripeCheckoutSessionSuccessUrl = renderUrl CheckoutSuccessR,
+              initiateStripeCheckoutSessionCanceledUrl = renderUrl CheckoutCanceledR
+            }
+    let stripeForm Monetisation {..} = $(widgetFile "stripe-form")
+    now <- liftIO getCurrentTime
+    withNavBar $(widgetFile "checkout")
 
 getCheckoutSuccessR :: Handler Html
 getCheckoutSuccessR = redirect AccountR
